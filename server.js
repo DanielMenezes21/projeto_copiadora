0acc5a212daeca5ae5b384b05e325d8e65841bc2
DanielMenezes21 <menezesd288@gmail.com>
2025-12-18 18:15:55 -0300

    correção da impressão e pagamento 2

--- server.js
+++ server.js
@@ -38,7 +38,13 @@ const upload = multer({ storage });
 
 
 // Inicializar Express
+
 const app = express();
+const cors = require("cors");
+app.use(cors({
+  origin: ["http://localhost:3000", "http://localhost:5500", "http://127.0.0.1:5500", "*"],
+  credentials: true,
+}));
 app.use(cookieParser());
 
 
